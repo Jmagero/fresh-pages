280cbf98e7fded8e74b11a880b69ff095aeb17af
Jocyline Magero <jmagero2017@gmail.com>
2021-03-05 21:30:01 +0300

    Associating model with shrine image attribute

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,6 @@ class Article < ApplicationRecord
   belongs_to :author, class_name'User'
   belongs_to :category
   has_many :votes
+  include ImageUploader::Attachment(:image)
+  validates :title, presence: true
 end
