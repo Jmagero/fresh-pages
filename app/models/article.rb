70af7bcef353dc4127f63cde68ced21964ad8c9f
Jocyline Magero <jmagero2017@gmail.com>
2021-03-05 21:45:46 +0300

    Add article_category association

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,11 @@
 class Article < ApplicationRecord
-  belongs_to :author, class_name'User'
-  belongs_to :category
-  has_many :votes
+  belongs_to :author, class_name: 'User'
+  has_many :votes, dependent: :destroy
+  has_many :article_categories
+  has_many :categories, through: :article_categories, dependent: :destroy
+
+  validates :title, presence: true, length: { in: 3..200 }
+  validates :text, presence: true
   include ImageUploader::Attachment(:image)
   validates :title, presence: true
 end
