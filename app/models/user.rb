bd423da8ef23c357a2028dc7e0ae6185b893e50f
Jocyline Magero <jmagero2017@gmail.com>
2021-03-07 00:51:58 +0300

    resolve conflicts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,5 @@
 class User < ApplicationRecord
-<<<<<<< HEAD
-    has_many :articles, dependent: :destroy
-    has_many :votes, dependent: :destroy
-    validates :name, presence: true
-=======
     has_many :articles
     has_many :votes
     validates :name, presence: true, uniqueness: true, length: { in: 2..50 }
->>>>>>> feature-login
 end
