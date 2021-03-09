a858375f0ade4c671fef71a36cc9dd421026977b
Jocyline Magero <jmagero2017@gmail.com>
2021-03-09 05:06:54 +0300

    Add votes_count column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_05_181059) do
+ActiveRecord::Schema.define(version: 2021_03_08_165604) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 2021_03_05_181059) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "image_data"
+    t.integer "votes_count"
     t.index ["author_id"], name: "index_articles_on_author_id"
   end
 
