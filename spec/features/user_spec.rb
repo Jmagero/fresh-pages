27d27250f53dd3521d2834d06e8d9ae3f2a74758
Jocyline Magero <jmagero2017@gmail.com>
2021-03-18 02:34:24 +0300

    Add tests for the features

---
+++ spec/features/user_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature "Users", type: :feature do
+  describe 'Navigate the webpage' do
+    it 'creates a new user and logs in' do
+      visit new_user_path
+      fill_in 'Name', with: 'Gui2'
+      click_on 'Create account'
+      expect(page).to have_content('Welcome to Fresh pages')
+      sleep(3)
+      click_on 'LOG IN'
+      fill_in 'Name', with: 'Gui2'
+      click_on 'Log in'
+      expect(page).to have_content('WRITE AN ARTICLE')
+    end
+  end
+end
