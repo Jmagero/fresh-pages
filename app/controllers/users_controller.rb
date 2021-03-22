06c846123f1105449e40949c53d10d904fa858d8
Jocyline Magero <jmagero2017@gmail.com>
2021-03-23 00:05:39 +0300

    Fix automatic sign in after signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,17 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:show]
   def new
     @user = User.new
   end
 
   def create
     @user = User.create(user_params)
-    if @user.save
-      flash[:success] = 'Welcome to Fresh pages'
-      redirect_to root_path
+    if @user.valid?
+      @user.save
+
+      session[:user_id] = @user.id
+      session[:name] = @user.name
+ 
+      redirect_to root_path, notice: "#{session[:name]} you were successfully created"
     else
       render 'new'
     end
