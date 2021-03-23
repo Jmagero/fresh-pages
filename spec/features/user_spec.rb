22e52edc8fc2239501ffd00a5676e67fc04700d0
Jocyline Magero <jmagero2017@gmail.com>
2021-03-24 02:50:39 +0300

    Add tests for invalid & blank name user

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -6,12 +6,7 @@ RSpec.feature 'Users', type: :feature do
       visit new_user_path
       fill_in 'Name', with: 'Gui2'
       click_on 'Register'
-      expect(page).to have_content('Welcome to Fresh pages')
-      sleep(3)
-      click_on 'LOG IN'
-      fill_in 'Name', with: 'Gui2'
-      click_on 'Log in'
-      expect(page).to have_content('WRITE AN ARTICLE')
+      expect(page).to have_content('Gui2 you were successfully created')
     end
 
     it 'does not login user with blank username' do
@@ -20,7 +15,17 @@ RSpec.feature 'Users', type: :feature do
       click_on 'LOG IN'
       fill_in 'Name', with: ' '
       click_on 'Log in'
-      expect(page).to have_content('WRITE AN ARTICLE')
+      expect(page).to have_content('Invalid user')
     end
+
+    it 'does not login user with invalid username' do
+      visit login_path
+      fill_in 'Name', with: 'Gui2'
+      click_on 'LOG IN'
+      fill_in 'Name', with: 'Norah'
+      click_on 'Log in'
+      expect(page).to have_content('Invalid user')
+    end
+
   end
 end
